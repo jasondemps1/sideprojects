dd63d8aed35de76595a64cae9f4d44783dec5c45
Jason Dempsey <jasondemps1@gmail.com>
2020-11-06 15:39:28 -0500

    Humidity in app. Added k8s manifest

--- kube-dash/js-client/src/App.js
+++ kube-dash/js-client/src/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-import { SensorRequest, SensorResponse } from "./sensorpb/sensor_pb"
+import { SensorRequest } from "./sensorpb/sensor_pb"
 import { SensorClient } from "./sensorpb/sensor_grpc_web_pb"
 
 var client = new SensorClient('http://localhost:8000')
 
 function App() {
   const [temp, setTemp] = useState(-9999)
+  const [humidity, setHumidity] = useState(-9999)
 
   const getTemp = () => {
-    console.log("called")
+    console.log("temp called")
 
     var sensorRequest = new SensorRequest()
     var stream = client.tempSensor(sensorRequest, {})
@@ -20,15 +21,32 @@ function App() {
     });
   };
 
+  const getHumidity = () => {
+    console.log("humidity called")
+
+    var sensorRequest = new SensorRequest()
+    var stream = client.humiditySensor(sensorRequest, {})
+
+    stream.on('data', function(response) {
+      setHumidity(response.getValue())
+    });
+  };
+
   useEffect(()=>{
     getTemp()
+  }, []);
+
+  useEffect(()=>{
+    getHumidity()
   }, []);
 
   return (
     <div>
       Temperature : {temp} F
+      <br />
+      Humidity : {humidity} %
     </div>
-  )
+  );
 }
 
 export default App;
